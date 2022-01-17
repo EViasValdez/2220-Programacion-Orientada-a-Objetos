de80eb579afa59fc7e0037e5d5a4a0d80d15b63f
EViasValdez <90350752+EViasValdez@users.noreply.github.com>
2022-01-17 09:44:35 -0600

    Add files via upload

---
+++ auto/auto/Auto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace auto
+{
+    class Auto
+    {
+        public int Id { get; set; }
+        public string NombreAuto { get; set; }
+        public string Marca { get; set; }
+        public string Modelo { get; set; }
+        public string Color { get; set; }
+
+    }
+}
