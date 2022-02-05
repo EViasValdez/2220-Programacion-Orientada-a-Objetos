28c45fea7371c96ed10aa35f9d46cfa88cbcecc7
Eduardo <edyvival@hotmail.com>
2022-02-05 13:38:03 -0600

    Add files via upload

---
+++ actividadPendiente00/actividadPendiente00/lenovo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace actividadPendiente00
+{
+    class lenovo
+    {
+        public string id;
+        public string marca;
+        public string modelo;
+        public string tipo;
+        public string sistemaoperativo;
+        public string capacidad;
+        public string memoriaram;
+        public string procesador;
+        public string tarjetagrafica;
+
+        public lenovo()
+        {
+            id = "ZNAS6Z2M";
+            marca = "Lenovo";
+            modelo = "IdeaPad 5i";
+            tipo = "Laptop";
+            sistemaoperativo = "Windows 10";
+            capacidad = "1TB en HDD y 256GBs en SSD";
+            memoriaram = "8 GBs";
+            procesador = "AMD Ryzen 5000";
+            tarjetagrafica = "AMD Radeon";
+        }
+    }
+}
