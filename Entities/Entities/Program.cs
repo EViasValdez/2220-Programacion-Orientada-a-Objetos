994b18b6371522cfa64208e749b30da418da7afe
Eduardo <edyvival@hotmail.com>
2022-02-10 11:08:38 -0600

    Add files via upload

--- Entities/Entities/Program.cs
+++ Entities/Entities/Program.cs
@@ -8,11 +8,11 @@ namespace Entities
         static void Main(string[] args)
         {
             List<Datos> listDatos = new List<Datos>();
-            listDatos.Add(new Datos { Id = 1, nombre = "Felix", aPaterno = "Amador", aMaterno = "Bosqueda", direccion = "Estado De Mexico, Mexico", telefono = "" });
-            listDatos.Add(new Datos { Id = 2, nombre = "Armando", aPaterno = "Vazquez Montes", aMaterno = "Villalba", direccion = "Guadalajara, Mexico", telefono = "" });
-            listDatos.Add(new Datos { Id = 3, nombre = "Maria", aPaterno = "Salgado", aMaterno = "Martinez", direccion = "Puebla, Mexico", telefono = "" });
-            listDatos.Add(new Datos { Id = 4, nombre = "Joselyn", aPaterno = "Perez Escobedo", aMaterno = "Rodriguez", direccion = "Queretaro, Mexico", telefono = "" });
-            listDatos.Add(new Datos { Id = 5, nombre = "Mauricio", aPaterno = "Hernandez", aMaterno = "Acevedo", direccion = "Hidalgo, Mexico", telefono = "" });
+            listDatos.Add(new Datos {Id = 1, nombre = "Felix", aPaterno = "Amador", aMaterno = "Bosqueda", direccion = "Estado De Mexico, Mexico", telefono = "" });
+            listDatos.Add(new Datos {Id = 2, nombre = "Armando", aPaterno = "Vazquez Montes", aMaterno = "Villalba", direccion = "Guadalajara, Mexico", telefono = "" });
+            listDatos.Add(new Datos {Id = 3, nombre = "Maria", aPaterno = "Salgado", aMaterno = "Martinez", direccion = "Puebla, Mexico", telefono = "" });
+            listDatos.Add(new Datos {Id = 4, nombre = "Joselyn", aPaterno = "Perez Escobedo", aMaterno = "Rodriguez", direccion = "Queretaro, Mexico", telefono = "" });
+            listDatos.Add(new Datos {Id = 5, nombre = "Mauricio", aPaterno = "Hernandez", aMaterno = "Acevedo", direccion = "Hidalgo, Mexico", telefono = "" });
         }
     }
 }
