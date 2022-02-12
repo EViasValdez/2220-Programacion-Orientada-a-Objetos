8c662e3bae8ce9f2c34edf1faf5369457bdc618e
Eduardo <edyvival@hotmail.com>
2022-02-12 13:38:04 -0600

    Add files via upload

---
+++ otraActividadOO/otraActividadOO/color1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace otraActividadOO
+{
+    class color1
+    {
+        public string rojo;
+        public string amarillo;
+        public string azul;
+        public string verde;
+        public color1()
+        {
+            rojo = "rgb(255, 0, 0)";
+            amarillo = "rgb(251, 255, 0)";
+            azul = "rgb(0, 8, 255)";
+            verde = "rgb(0, 255, 30)";
+        }
+    }
+}
---
+++ otraActividadOO/otraActividadOO/color2.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace otraActividadOO
+{
+    class color2
+    {
+        public string rojogarante;
+        public string amarillooro;
+        public string azulrey;
+        public string verdeoscuro;
+        public color2()
+        {
+            rojogarante = "rgb(184, 0, 0)";
+            amarillooro = "rgb(255, 215, 0)";
+            azulrey = "rgb(0, 3, 77)";
+            verdeoscuro = "rgb(28, 168, 0)";
+        }
+    }
+}
