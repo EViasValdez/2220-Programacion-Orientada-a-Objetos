b33d40ccc79a545126e7cf08bff62ca19467b261
EViasValdez <eduardoviasvaldez@yahoo.com.mx>
2022-11-15 22:33:12 -0600

    Add files via upload

---
+++ Entities1/Entities1/Datos.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities1
+{
+    class Datos
+    {
+        public int Id { get; set; }
+        public string nombre { get; set; }
+        public string aPaterno { get; set; }
+        public string aMaterno { get; set; }
+        public string direccion { get; set; }
+        public string telefono { get; set; }
+    }
+}
