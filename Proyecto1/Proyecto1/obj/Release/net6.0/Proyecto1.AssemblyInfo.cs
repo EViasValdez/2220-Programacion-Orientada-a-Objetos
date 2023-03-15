4994efda472ea7913f93e7550d93266558b08a91
EViasValdez <eduardoviasvaldez@yahoo.com.mx>
2023-03-14 21:48:11 -0600

    Add files via upload

--- Proyecto1/Proyecto1/obj/Release/net6.0/Proyecto1.AssemblyInfo.cs
+++ Proyecto1/Proyecto1/obj/Release/net6.0/Proyecto1.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Agregacion")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Agregacion")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Agregacion")]
+[assembly: System.Reflection.AssemblyProductAttribute("Proyecto1")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Proyecto1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
