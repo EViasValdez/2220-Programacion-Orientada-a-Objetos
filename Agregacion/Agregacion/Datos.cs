6a5950eadb5c17edd6865fdd0e1b3d409ad34aab
EViasValdez <eduardoviasvaldez@yahoo.com.mx>
2023-03-14 21:54:25 -0600

    Add files via upload

---
+++ Agregacion/Agregacion/Datos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agregacion
+{
+    class Datos
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; }
+        public string APaterno { get; set; }
+        public string AMaterno { get; set; }
+    }
+}
---
+++ Agregacion/Agregacion/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agregacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona Pers = new Persona();
+        }
+    }
+}
