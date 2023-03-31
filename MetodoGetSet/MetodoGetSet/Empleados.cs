938cfe9652c000564c53926dee96e14fbdac1448
EViasValdez <eduardoviasvaldez@yahoo.com.mx>
2023-03-30 21:44:07 -0600

    Add files via upload

---
+++ MetodoGetSet/MetodoGetSet/Empleados.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace metodoGetSet
+{
+    class Empleados
+    {
+        // public int id { get; set; }
+        // private string _nombre;
+        // public string nombre
+        // {
+        // get
+        // {
+        //     if (_nombre == "Juanito")
+        //     {
+        //         _nombre = "Juanito es adoptado";
+        //     }
+        //     else
+        //     {
+        //         _nombre = "Juanito no es adoptado";
+        //     }
+        //     return _nombre;
+        // }
+        // set { _nombre = value; }
+        // }
+        // }
+    }
+}
---
+++ MetodoGetSet/MetodoGetSet/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace metodoGetSet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Metodo m = new Metodo();
+            // m.setValor("Eduardo");
+            // Console.WriteLine(m.getValor());
+            // Empleados e = new Empleados();
+            // e.nombre = "Javier";
+            // Console.ReadKey();
+
+            Practica Pract = new Practica();
+
+            Console.WriteLine("Ingrese el Id");
+            Pract.Id = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el nombre");
+            Pract.Nombre = (Console.ReadLine());
+
+            Console.WriteLine("Ingrese el total");
+            Pract.Total = float.Parse(Console.ReadLine());
+            Pract.Iva = Pract.Total;
+
+            Console.WriteLine("----------------");
+            Console.WriteLine("El id es: " + Pract.Id + " con nombre: " + Pract.Nombre);
+            Console.WriteLine("Con un total de: " + Pract.Total);
+            Console.WriteLine("Un iva de: " + Pract.Iva);
+            Console.WriteLine("El total iva es: " + Pract.TotalIva);
+            Console.WriteLine("El descuento es: " + Pract.descuento);
+            Console.WriteLine(Pract.Iva);
+        }
+    }
+}
