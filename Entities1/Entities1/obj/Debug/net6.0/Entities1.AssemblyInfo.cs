2890bcff850e064c661cea83d8d3bc67f4c1c817
Eduardo <eduardoviasvaldez@yahoo.com.mx>
2024-01-05 10:13:09 -0600

    Add files via upload

--- Entities1/Entities1/obj/Debug/net6.0/Entities1.AssemblyInfo.cs
+++ Entities1/Entities1/obj/Debug/net6.0/Entities1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entities1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dbe5477045ee536c440ce9311d143490147a40df")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entities1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entities1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
