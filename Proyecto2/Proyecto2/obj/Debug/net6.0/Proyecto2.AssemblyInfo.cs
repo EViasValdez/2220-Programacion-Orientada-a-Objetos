179ea6e5ac9506cc23fa16b09bdec1010bb40345
Eduardo <eduardoviasvaldez@yahoo.com.mx>
2024-03-12 20:33:09 -0600

    Add files via upload

--- Proyecto2/Proyecto2/obj/Debug/net6.0/Proyecto2.AssemblyInfo.cs
+++ Proyecto2/Proyecto2/obj/Debug/net6.0/Proyecto2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85960c7fe515547815612c3f499f5cb7cb5e6b6f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de1d16ee3dc094fa4b26c09a84c354fd0b98de3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Proyecto2/Proyecto2/obj/Release/net6.0/Proyecto2.AssemblyInfo.cs
+++ Proyecto2/Proyecto2/obj/Release/net6.0/Proyecto2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5a5e4c839350d04f499344f473294aa85fd293e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+de1d16ee3dc094fa4b26c09a84c354fd0b98de3f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
