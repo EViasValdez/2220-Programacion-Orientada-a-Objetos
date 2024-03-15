6c9598d90affd5b11a308873d87fdb0b79704f31
Eduardo <eduardoviasvaldez@yahoo.com.mx>
2024-03-14 20:39:38 -0600

    Add files via upload

--- ActividadPendienteOO/ActividadPendienteOO/obj/Release/net6.0/ActividadPendienteOO.AssemblyInfo.cs
+++ ActividadPendienteOO/ActividadPendienteOO/obj/Release/net6.0/ActividadPendienteOO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ActividadPendienteOO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c5a5e4c839350d04f499344f473294aa85fd293e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+179ea6e5ac9506cc23fa16b09bdec1010bb40345")]
 [assembly: System.Reflection.AssemblyProductAttribute("ActividadPendienteOO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ActividadPendienteOO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
