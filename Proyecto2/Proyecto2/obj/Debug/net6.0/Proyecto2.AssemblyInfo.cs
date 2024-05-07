d43de383aed7ca11cf638aacb8db598a802fcb3a
Eduardo <eduardoviasvaldez@yahoo.com.mx>
2024-05-07 10:27:44 -0600

    Add files via upload

--- Proyecto2/Proyecto2/obj/Debug/net6.0/Proyecto2.AssemblyInfo.cs
+++ Proyecto2/Proyecto2/obj/Debug/net6.0/Proyecto2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93fb37f4189b7e9b5fe98790d025541ab5739c08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+baa37fe5a6bdeadf7eb344db9d198b02bc2ae3de")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
