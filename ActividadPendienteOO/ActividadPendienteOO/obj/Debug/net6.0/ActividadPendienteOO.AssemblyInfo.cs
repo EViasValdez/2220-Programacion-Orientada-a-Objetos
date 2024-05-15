11bd8d63b59bcc04d6372c101381c680c524561c
Eduardo <eduardoviasvaldez@yahoo.com.mx>
2024-05-15 09:15:25 -0600

    Add files via upload

--- ActividadPendienteOO/ActividadPendienteOO/obj/Debug/net6.0/ActividadPendienteOO.AssemblyInfo.cs
+++ ActividadPendienteOO/ActividadPendienteOO/obj/Debug/net6.0/ActividadPendienteOO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ActividadPendienteOO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+93fb37f4189b7e9b5fe98790d025541ab5739c08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+32c7239a182c0bc60fce840fa52403b60460531f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ActividadPendienteOO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ActividadPendienteOO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
