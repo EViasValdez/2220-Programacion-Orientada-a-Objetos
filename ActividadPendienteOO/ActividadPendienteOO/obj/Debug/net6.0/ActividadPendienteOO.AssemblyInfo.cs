9736bd4e2a691564e398c0737affabfce4efdf9f
Eduardo <eduardoviasvaldez@yahoo.com.mx>
2024-07-17 17:16:11 -0600

    Add files via upload

--- ActividadPendienteOO/ActividadPendienteOO/obj/Debug/net6.0/ActividadPendienteOO.AssemblyInfo.cs
+++ ActividadPendienteOO/ActividadPendienteOO/obj/Debug/net6.0/ActividadPendienteOO.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ActividadPendienteOO")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb8c2f9d1ceb5af60347784a880544a4bb1e1704")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63af9fe5965c8df4fcf1aa0744fef2346821405d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ActividadPendienteOO")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ActividadPendienteOO")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- Proyecto1/Proyecto1/obj/Release/net6.0/Proyecto1.AssemblyInfo.cs
+++ Proyecto1/Proyecto1/obj/Release/net6.0/Proyecto1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proyecto1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb8c2f9d1ceb5af60347784a880544a4bb1e1704")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63af9fe5965c8df4fcf1aa0744fef2346821405d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proyecto1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proyecto1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
