f046ce3e15556975e457906a5fdff4f9be74f187
Eduardo <eduardoviasvaldez@yahoo.com.mx>
2024-07-17 20:45:30 -0600

    Add files via upload

--- Calculadora1/Calculadora1/obj/Debug/net6.0/Calculadora1.AssemblyInfo.cs
+++ Calculadora1/Calculadora1/obj/Debug/net6.0/Calculadora1.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Calculadora1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63af9fe5965c8df4fcf1aa0744fef2346821405d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b33ed959a3f4b2807da3440775a114a4508578df")]
 [assembly: System.Reflection.AssemblyProductAttribute("Calculadora1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Calculadora1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
