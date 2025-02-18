3acc1869f41fec8e2768a3e423dfdacb89b9ad9e
Eduardo <eduardoviasvaldez@yahoo.com.mx>
2025-02-17 22:14:46 -0600

    Add files via upload

--- Listas/Listas/obj/Release/net8.0/Listas.AssemblyInfo.cs
+++ Listas/Listas/obj/Release/net8.0/Listas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Listas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b948fdd92c4d95281a4096aee7ca94fea0274b4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+833211ba0f6a8427ab153c449387a6ad293db7ff")]
 [assembly: System.Reflection.AssemblyProductAttribute("Listas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Listas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
